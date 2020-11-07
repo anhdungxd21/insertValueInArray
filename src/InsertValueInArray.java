d3949330d251f70453687acae8c09517718582a7
= <=>
2020-11-07 08:50:56 +0700

    done

---
+++ src/InsertValueInArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class InsertValueInArray {
+    public static void main(String[] args) {
+        int[] array = {10,4,6,7,8,0,0,0,0,0};
+        Scanner input = new Scanner(System.in);
+        printArray(array);
+        System.out.print("Enter the value: ");
+        int value = input.nextInt();
+        System.out.print("Enter index: ");
+        int index = input.nextInt();
+        insertValueInArray(array,value,index);
+        printArray(array);
+    }
+
+    public static void insertValueInArray(int[] array,int value, int index) {
+        if(index > array.length-1){
+            System.out.println("Out of Range");
+            return;
+        }else{
+            for (int i = array.length-2; i >= index; i--) {
+                array[i+1] = array[i];
+            }
+            array[index] = value;
+        }
+
+    }
+    public static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf(array[i] + "\t");
+        }
+        System.out.println();
+    }
+}
